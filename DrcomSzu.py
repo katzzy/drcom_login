af888e0798b634a953ceb2de7ed9c5775bd40222
Korbin Ding <jianbind@foxmail.com>
2024-01-14 00:30:41 +0800

    feat: :sparkles: add set the config file entry

--- DrcomSzu.py
+++ DrcomSzu.py
@@ -23,17 +23,21 @@ class DrcomSzu(object):
         if not os.path.exists("config.yml"):
             print("配置文件不存在！")
             print("正在创建配置文件...")
-            print("请输入校园网账号：")
-            username = input()
-            print("请输入校园网密码：")
-            password = input()
-            with open("config.yml", "w") as ymlfile:
-                cfg = {"username": username, "password": password}
-                yaml.dump(cfg, ymlfile)
-            print("配置文件创建成功！")
+            self.set_config()
         with open("config.yml", "r") as ymlfile:
             config = yaml.safe_load(ymlfile)
         return config
+
+    @staticmethod
+    def set_config():
+        print("请输入校园网账号：")
+        username = input()
+        print("请输入校园网密码：")
+        password = input()
+        with open("config.yml", "w") as ymlfile:
+            cfg = {"username": username, "password": password}
+            yaml.dump(cfg, ymlfile)
+        print("配置文件更新成功！")
 
     def __check_internet_connection(self):
         r = requests.get(self.get_url("check")).text
@@ -70,20 +74,14 @@ class DrcomSzu(object):
         sleep_time = input("请输入自动检测间隔时间(单位：分钟):")
         print("程序将每隔" + sleep_time + "分钟自动检测网络连接状态并登录!")
         print("按下Ctrl+C可退出程序!")
-        try:
-            while True:
-                self.login()
-                time.sleep(60 * int(sleep_time))
-        except KeyboardInterrupt:
-            print("按下了Ctrl+C!")
-            print("程序执行完成！请按下回车键退出...")
-            input()
-            exit(0)
+        while True:
+            self.login()
+            time.sleep(60 * int(sleep_time))
 
     def get_url(self, type):
         raise NotImplementedError
 
-    def __login_function(self):
+    def login_function(self):
         raise NotImplementedError
 
 
@@ -148,12 +146,15 @@ def main():
     print("欢迎使用DrcomSzu！")
     type = input(
         "请选择网络类型：\n"
+        "0. 设置账号密码\n"
         "1. 宿舍网络(单次登录)\n"
         "2. 教学区网络(单次登录)\n"
         "3. 宿舍网络(自动续期)\n"
         "4. 教学区网络(自动续期)\n"
     )
     match type:
+        case "0":
+            DrcomSzu.set_config()
         case "1":
             drcom_szu_dormitory = DrcomSzuDormitory()
             drcom_szu_dormitory.login()
@@ -167,14 +168,23 @@ def main():
             drcom_szu_office = DrcomSzuOffice()
             drcom_szu_office.login_auto()
         case _:
-            print("输入错误！")
-            print("程序执行完成！请按下回车键退出...")
-            input()
-            exit(0)
+            raise ValueError("Invalid type")
 
 
 if __name__ == "__main__":
-    main()
-    print("程序执行完成！请按下回车键退出...")
-    input()
-    exit(0)
+    try:
+        main()
+    except ValueError:
+        print("无效的类型！")
+    except KeyboardInterrupt:
+        print("按下了Ctrl+C!")
+    except requests.exceptions.ConnectionError:
+        print("网络连接错误！")
+        print("可能的原因如下：")
+        print("1. 未连接到校园网")
+        print("2. 校园网服务器故障")
+        print("3. 在宿舍区尝试登录教学区网络")
+    finally:
+        print("程序执行完成！请按下回车键退出...")
+        input()
+        exit(0)
