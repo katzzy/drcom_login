c03fe5c9efaa43c54a97bc6fab7d9c627f6abb1a
Korbin Ding <jianbind@foxmail.com>
2024-01-17 13:12:15 +0800

    perf: :zap: adjust the implementation to test network connectivity

--- DrcomSzu.py
+++ DrcomSzu.py
@@ -8,14 +8,26 @@ import requests
 
 class DrcomSzu(object):
     def __init__(self):
-        self.__set_working_directory()
-        self._config = self.__get_config()
+        self._config = None
         self._headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
             " AppleWebKit/537.36 (KHTML, like Gecko)"
             " Chrome/120.0.0.0 Safari/537.36"
         }
         self.__internet_connectivity = False
+        self.__set_working_directory()
+        self.__get_config()
+
+    @staticmethod
+    def set_config():
+        print("请输入校园网账号：")
+        username = input()
+        print("请输入校园网密码：")
+        password = input()
+        with open("config.yml", "w") as ymlfile:
+            cfg = {"username": username, "password": password}
+            yaml.dump(cfg, ymlfile)
+        print("配置文件更新成功！")
 
     @staticmethod
     def __set_working_directory():
@@ -33,18 +45,7 @@ class DrcomSzu(object):
             self.set_config()
         with open("config.yml", "r") as ymlfile:
             config = yaml.safe_load(ymlfile)
-        return config
-
-    @staticmethod
-    def set_config():
-        print("请输入校园网账号：")
-        username = input()
-        print("请输入校园网密码：")
-        password = input()
-        with open("config.yml", "w") as ymlfile:
-            cfg = {"username": username, "password": password}
-            yaml.dump(cfg, ymlfile)
-        print("配置文件更新成功！")
+        self._config = config
 
     def get_public_ip(self):
         with requests.Session() as s:
@@ -72,25 +73,17 @@ class DrcomSzu(object):
                 print("DNS设置错误！")
 
     def __check_internet_connection(self):
-        r = requests.get(self._get_check_url(), headers=self._headers).text
-        self.__internet_connectivity = (
-            True
-            if re.findall(r"<title>(.*?)</title>", r)[0] == "注销页"
-            or re.findall(r"<title>(.*?)</title>", r)[0] == "Drcom PC注销页"
-            else False
-        )
-
-    def get_ip(self):
-        r = requests.get(self._get_check_url(), headers=self._headers).text
-        self.__check_internet_connection()
-        if self.__internet_connectivity:
-            ip = re.findall(r"v4ip='(.*?)'", r)[0]
-            print("校内IP地址：" + ip)
-            return ip
+        if os.name == "nt":
+            exit_code = os.system("ping 183.2.172.42 -w 1000 -n 1 > nul")
+        elif os.name == "posix":
+            exit_code = os.system("ping 183.2.172.42 -W 1 -c 1 > /dev/null")
         else:
-            ip = re.findall(r"v46ip='(.*?)'", r)[0]
-            print("校内IP地址：" + ip)
-            return ip
+            print("未知系统！请自行修改代码后运行！")
+            exit(1)
+        if exit_code == 0:
+            self.__internet_connectivity = True
+        else:
+            self.__internet_connectivity = False
 
     def login(self):
         self.__check_internet_connection()
@@ -108,16 +101,12 @@ class DrcomSzu(object):
             self._login_function()
 
     def login_auto(self):
-        print("注意:请不要设置过短的间隔时间，至少大于5分钟!!!")
         sleep_time = input("请输入自动检测间隔时间(单位：分钟):")
         print("程序将每隔" + sleep_time + "分钟自动检测网络连接状态并登录!")
         print("按下Ctrl+C可退出程序!")
         while True:
             self.login()
             time.sleep(60 * int(sleep_time))
-
-    def _get_check_url(self):
-        raise NotImplementedError
 
     def _login_function(self):
         raise NotImplementedError
@@ -126,11 +115,12 @@ class DrcomSzu(object):
 class DrcomSzuDormitory(DrcomSzu):
     def __init__(self):
         super().__init__()
-        self.__check_url = "http://172.30.255.42"
-        self.__login_url = "http://172.30.255.42:801/eportal/portal/login"
 
-    def _get_check_url(self):
-        return self.__check_url
+    def __get_ip(self):
+        r = requests.get("http://172.30.255.42", headers=self._headers).text
+        ip = re.findall(r"v46ip='(.*?)'", r)[0]
+        print("校内IP地址：" + ip)
+        return ip
 
     def _login_function(self):
         payload = {
@@ -138,7 +128,7 @@ class DrcomSzuDormitory(DrcomSzu):
             "login_method": "1",
             "user_account": ",0," + self._config["username"],
             "user_password": self._config["password"],
-            "wlan_user_ip": self.get_ip(),
+            "wlan_user_ip": self.__get_ip(),
             "wlan_user_ipv6": "",
             "wlan_user_mac": "000000000000",
             "wlan_ac_ip": "",
@@ -150,7 +140,9 @@ class DrcomSzuDormitory(DrcomSzu):
             "lang": "en",
         }
         drcom_res = requests.get(
-            self.__login_url, params=payload, headers=self._headers
+            "http://172.30.255.42:801/eportal/portal/login",
+            params=payload,
+            headers=self._headers,
         )
         result = re.findall(r"\"result\":(\d)", drcom_res.text)[0]
         if result == "1":
@@ -162,11 +154,6 @@ class DrcomSzuDormitory(DrcomSzu):
 class DrcomSzuOffice(DrcomSzu):
     def __init__(self):
         super().__init__()
-        self.__check_url = "https://drcom.szu.edu.cn/"
-        self.__login_url = "https://drcom.szu.edu.cn/a70.htm"
-
-    def _get_check_url(self):
-        return self.__check_url
 
     def _login_function(self):
         drcom_form = {
@@ -175,7 +162,9 @@ class DrcomSzuOffice(DrcomSzu):
             "0MKKey": "%B5%C7%A1%A1%C2%BC",
         }
         drcom_res = requests.post(
-            self.__login_url, data=drcom_form, headers=self._headers
+            "https://drcom.szu.edu.cn/a70.htm",
+            data=drcom_form,
+            headers=self._headers,
         )
         title = re.findall(r"<title>(.*?)</title>", drcom_res.text)[0]
         if title == "Drcom PC登陆成功页":
@@ -204,9 +193,8 @@ def drcom_user_interface():
             "2. 教学区网络(单次登录)\n"
             "3. 宿舍网络(自动续期)\n"
             "4. 教学区网络(自动续期)\n"
-            "5. 查询校内ip(仅宿舍网络)\n"
-            "6. 查询公网ip\n"
-            "7. 查询DNS地址\n"
+            "5. 查询公网ip\n"
+            "6. 查询DNS地址\n"
             "100. 退出程序\n"
             "请输入功能编号："
         )
@@ -223,10 +211,8 @@ def drcom_user_interface():
             case "4":
                 drcom_login(DrcomSzuOffice(), True)
             case "5":
-                DrcomSzuDormitory().get_ip()
-            case "6":
                 DrcomSzu().get_public_ip()
-            case "7":
+            case "6":
                 DrcomSzu().get_dns_address()
             case "100":
                 break
